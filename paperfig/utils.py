ad9427a16b777c96fd7185bc9b7c8cd7ce87f11f
AdamsMP <87016586+AdamsMP92@users.noreply.github.com>
2025-11-18 13:06:53 +0100

    Update utils.py

--- paperfig/utils.py
+++ paperfig/utils.py
@@ -33,3 +33,60 @@ def add_reference_axes(plotter, length=0.5, radius=0.02, offset=0.8):
             shaft_radius=radius, scale=length
         )
         plotter.add_mesh(arrow, color=colors[key], smooth_shading=True)
+
+
+def apply_tick_style(
+        ax,
+        show_ticks=True,
+        ticks_fontsize=6,
+        major_tick_length=2.5,
+        major_tick_width=0.45,
+        minor_tick_length=1.5,
+        minor_tick_width=0.35,
+        xticks=None,
+        yticks=None,
+        xticklabels=None,
+        yticklabels=None
+):
+    """Unified tick styling for 1D and 2D plots."""
+
+    if not show_ticks:
+        ax.set_xticks([])
+        ax.set_yticks([])
+        return ax
+
+    # Set tick positions
+    if xticks is not None:
+        ax.set_xticks(xticks)
+        if xticklabels is not None:
+            ax.set_xticklabels(xticklabels, fontsize=ticks_fontsize)
+
+    if yticks is not None:
+        ax.set_yticks(yticks)
+        if yticklabels is not None:
+            ax.set_yticklabels(yticklabels, fontsize=ticks_fontsize)
+
+    # Major ticks
+    ax.tick_params(
+        which="major",
+        direction="in",
+        labelsize=ticks_fontsize,
+        length=major_tick_length,
+        width=major_tick_width,
+        top=True,
+        right=True
+    )
+
+    # Minor ticks
+    ax.tick_params(
+        which="minor",
+        direction="in",
+        length=minor_tick_length,
+        width=minor_tick_width,
+        top=True,
+        right=True
+    )
+
+    ax.minorticks_on()
+    return ax
+
